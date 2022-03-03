ed77fcff646384c74770e7a9e4ca019754aad455
kkb-mmmut <kkb.mmmut@gmail.com>
2022-03-03 18:44:55 +0530

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <h1>Welcome to React Vite Micro App!</h1>
+      <p>
+        <marquee>Hard to get more minimal than this React app.</marquee>
+      </p>
+    </>
+  );
+};
+export default App;
